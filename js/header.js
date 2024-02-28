b8067d96c0740e1e6a6e189190ffa42b51bca8fa
Dovydas-G <146329425+Dovydas-G@users.noreply.github.com>
2024-02-28 11:55:52 +0200

    Added cart

--- js/header.js
+++ js/header.js
@@ -1,13 +1,17 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
-    const href = location.hostname === 'localhost' ? '/' :  !location.pathname ? '../' : './';
-    // const href = window.location.pathname ? '../' : './';
+    const localhost = 'http://localhost:4815';
+    const github = 'https://dovydas-g.github.io/_48-GRUPE-DOM-EVENTS_/'
+    const href = location.hostname === 'localhost' ? localhost : github;
+    
+
     const HTML = `
         <a href="${href}">Home</a>
         <a href="${href}single-button-click">Single button click</a>
         <a href="${href}counter/">Counter</a>
         <a href="${href}basketball/">Basketball</a>
         <a href="${href}virykle/">Virykle</a>
+        <a href="${href}/cart/">Prekiu krepselis</a>
     `;
     navDOM.innerHTML = HTML;
 }
